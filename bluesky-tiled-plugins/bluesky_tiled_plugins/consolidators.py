1cc76866fb69845ba455a103a08423acf250c626
Eugene M <ymatviych@bnl.gov>
2025-10-29 19:44:38 -0400

    MNT: lint

--- bluesky-tiled-plugins/bluesky_tiled_plugins/consolidators.py
+++ bluesky-tiled-plugins/bluesky_tiled_plugins/consolidators.py
@@ -230,10 +230,10 @@ class ConsolidatorBase:
                     list_summands(self.datum_shape[0], self.chunk_shape[0], repeat=self._num_rows),
                     *[
                         list_summands(ddim, cdim)
-                        for ddim, cdim in zip(self.shape[1 : len(self.chunk_shape)], self.chunk_shape[1:])
+                        for ddim, cdim in zip(self.shape[1 : len(self.chunk_shape)], self.chunk_shape[1:])  # noqa
                     ],
                 )
-            return result + tuple((d,) for d in self.shape[len(self.chunk_shape) :])
+            return result + tuple((d,) for d in self.shape[len(self.chunk_shape) :])  # noqa: E203
 
         # If chunk shape is longer than the total shape dimensions, raise an error
         else:
